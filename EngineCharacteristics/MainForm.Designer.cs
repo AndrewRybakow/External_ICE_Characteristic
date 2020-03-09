595ee5f7176ecb5f6b6d72d83e7e6ec31af6d2c7
Andrew Rybakow <AndrewRybakow@outlook.com>
2020-03-09 20:31:35 +0300

    Rename Form1 to MainForm

--- EngineCharacteristics/MainForm.Designer.cs
+++ EngineCharacteristics/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EngineCharacteristics
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -321,7 +321,7 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Исходные данные:";
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -333,7 +333,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Внешняя скоростная характеристика ДВС";
             this.tableLayoutPanel1.ResumeLayout(false);
